ded5021ee63a54785cde31a395096437b1a4cddd
soblvsk <SobVitalya@yandex.ru>
2024-04-03 16:28:44 +0700

    feat: add starting new portfolio based on @antfu

---
+++ vite.config.ts
@@ -0,0 +1,191 @@
+import { basename, resolve } from 'node:path';
+import { defineConfig } from 'vite';
+import fs from 'fs-extra';
+import Pages from 'vite-plugin-pages';
+import Inspect from 'vite-plugin-inspect';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+import Components from 'unplugin-vue-components/vite';
+import Markdown from 'unplugin-vue-markdown/vite';
+import Vue from '@vitejs/plugin-vue';
+import matter from 'gray-matter';
+import AutoImport from 'unplugin-auto-import/vite';
+import anchor from 'markdown-it-anchor';
+import LinkAttributes from 'markdown-it-link-attributes';
+import GitHubAlerts from 'markdown-it-github-alerts';
+import UnoCSS from 'unocss/vite';
+import SVG from 'vite-svg-loader';
+import MarkdownItShiki from '@shikijs/markdown-it';
+import { rendererRich, transformerTwoslash } from '@shikijs/twoslash';
+import MarkdownItMagicLink from 'markdown-it-magic-link';
+
+// @ts-expect-error missing types
+import TOC from 'markdown-it-table-of-contents';
+import { slugify } from './scripts/slugify';
+
+const promises: Promise<any>[] = [];
+
+export default defineConfig({
+  resolve: {
+    alias: [
+      { find: '~/', replacement: `${resolve(__dirname, 'src')}/` },
+    ],
+  },
+  optimizeDeps: {
+    include: [
+      'vue',
+      'vue-router',
+      '@vueuse/core',
+      'dayjs',
+      'dayjs/plugin/localizedFormat',
+    ],
+  },
+  plugins: [
+    UnoCSS(),
+
+    Vue({
+      include: [/\.vue$/, /\.md$/],
+      script: {
+        defineModel: true,
+      },
+    }),
+
+    Pages({
+      extensions: ['vue', 'md'],
+      dirs: 'pages',
+      extendRoute(route) {
+        const path = resolve(__dirname, route.component.slice(1));
+
+        if (!path.includes('projects.md') && path.endsWith('.md')) {
+          const md = fs.readFileSync(path, 'utf-8');
+          const { data } = matter(md);
+          route.meta = Object.assign(route.meta || {}, { frontmatter: data });
+        }
+
+        return route;
+      },
+    }),
+
+    Markdown({
+      wrapperComponent: 'WrapperPost',
+      wrapperClasses: (id, code) => code.includes('@layout-full-width')
+        ? ''
+        : 'prose m-auto slide-enter-content',
+      headEnabled: true,
+      exportFrontmatter: false,
+      exposeFrontmatter: false,
+      exposeExcerpt: false,
+      markdownItOptions: {
+        quotes: '""\'\'',
+      },
+      async markdownItSetup(md) {
+        md.use(await MarkdownItShiki({
+          themes: {
+            dark: 'vitesse-dark',
+            light: 'vitesse-light',
+          },
+          defaultColor: false,
+          cssVariablePrefix: '--s-',
+          transformers: [
+            transformerTwoslash({
+              explicitTrigger: true,
+              renderer: rendererRich(),
+            }),
+          ],
+        }));
+
+        md.use(anchor, {
+          slugify,
+          permalink: anchor.permalink.linkInsideHeader({
+            symbol: '#',
+            renderAttrs: () => ({ 'aria-hidden': 'true' }),
+          }),
+        });
+
+        md.use(LinkAttributes, {
+          matcher: (link: string) => /^https?:\/\//.test(link),
+          attrs: {
+            target: '_blank',
+            rel: 'noopener',
+          },
+        });
+
+        md.use(TOC, {
+          includeLevel: [1, 2, 3, 4],
+          slugify,
+          containerHeaderHtml: '<div class="table-of-contents-anchor"><div class="i-ri-menu-2-fill" /></div>',
+        });
+
+        md.use(MarkdownItMagicLink);
+
+        md.use(GitHubAlerts);
+      },
+
+      frontmatterPreprocess(frontmatter, options, id, defaults) {
+        (() => {
+          if (!id.endsWith('.md'))
+            return;
+          const route = basename(id, '.md');
+          if (route === 'index' || frontmatter.image || !frontmatter.title)
+            return;
+          const path = 'og.png';
+
+          frontmatter.image = `https://soblvsk.netlify.app/${path}`;
+        })();
+        const head = defaults(frontmatter, options);
+        return { head, frontmatter };
+      },
+    }),
+
+    AutoImport({
+      imports: [
+        'vue',
+        'vue-router',
+        '@vueuse/core',
+      ],
+    }),
+
+    Components({
+      extensions: ['vue', 'md'],
+      dts: true,
+      include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
+      resolvers: [
+        IconsResolver({
+          componentPrefix: '',
+        }),
+      ],
+    }),
+
+    Inspect(),
+
+    Icons({
+      defaultClass: 'inline',
+      defaultStyle: 'vertical-align: sub;',
+    }),
+
+    SVG({
+      svgo: false,
+      defaultImport: 'url',
+    }),
+
+    {
+      name: 'await',
+      async closeBundle() {
+        await Promise.all(promises);
+      },
+    },
+  ],
+
+  build: {
+    rollupOptions: {
+      onwarn(warning, next) {
+        if (warning.code !== 'UNUSED_EXTERNAL_IMPORT')
+          next(warning);
+      },
+    },
+  },
+
+  ssgOptions: {
+    formatting: 'minify',
+  },
+});
